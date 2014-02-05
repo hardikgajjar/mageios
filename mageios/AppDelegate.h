95933c5238c52510c340962f9bae677f0577aef6
KTPL - Mobile Development <jiten@Shalin-Patels-MacBook-Air.local>
2014-02-05 13:55:38 +0530

    Initial Commit

---
+++ mageios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mageios
+//
+//  Created by KTPL - Mobile Development on 05/02/14.
+//  Copyright (c) 2014 KTPL - Mobile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
